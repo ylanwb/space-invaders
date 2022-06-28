fc658cb04ec94ea7acee80af019ce60c7cc3e138
Chingun <zolboo@Chinguns-iMac.local>
2022-06-28 04:48:56 -0700

    bugs

--- gamePage/game.js
+++ gamePage/game.js
@@ -12,8 +12,6 @@ class Player {
       y: 0,
     };
 
-    this.rotation = 0;
-
     const image = new Image();
     image.src = "./img/spaceship.png";
     image.onload = () => {
@@ -30,6 +28,7 @@ class Player {
   draw() {
     // c.fillStyle = "red";
     // c.fillRect(this.position.x, this.position.y, this.width, this.height);
+
     if (this.image)
       c.drawImage(
         this.image,
@@ -40,13 +39,108 @@ class Player {
       );
   }
   update() {
-    this.draw();
-    this.position.x += this.velocity.x;
+    if (this.image) {
+      this.draw();
+      this.position.x += this.velocity.x;
+    }
   }
 }
 
-const player = new Player();
+class Projectile {
+  constructor({ position, velocity }) {
+    this.position = position;
+    this.velocity = velocity;
 
+    this.radius = 3;
+  }
+
+  draw() {
+    c.beginPath();
+    c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
+    c.fillStyle = "red";
+    c.fill();
+    c.closePath();
+  }
+  update() {
+    this.draw();
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+  }
+}
+
+class Invader {
+  constructor({position}) {
+    this.velocity = {
+      x: 0,
+      y: 0,
+    };
+
+    const image = new Image();
+    image.src = "./img/invader.png";
+    image.onload = () => {
+      const scale = 2;
+      this.image = image;
+      this.width = image.width * scale;
+      this.height = image.height * scale;
+      this.position = {
+        x: position.x,
+        y: position.y
+      };
+    };
+  }
+
+  draw() {
+    // c.fillStyle = "red";
+    // c.fillRect(this.position.x, this.position.y, this.width, this.height);
+
+    if (this.image)
+      c.drawImage(
+        this.image,
+        this.position.x,
+        this.position.y,
+        this.width,
+        this.height
+      );
+  }
+  update() {
+    if (this.image) {
+      this.draw();
+      this.position.x += this.velocity.x;
+      this.position.y += this.velocity.y;
+    }
+  }
+}
+
+class Grid {
+  constructor() {
+    this.position = {
+      x: 0,
+      y: 0,
+    };
+
+    this.velocity = {
+      x: 0,
+      y: 0,
+    };
+
+    this.invaders = [new Invader()];
+
+    for (let i = 0; i < 9; i++) {
+      this.invaders.push(
+        new Invader({position: {
+          x:0,
+          y:0
+        }})
+      );
+    }
+    console.log(this.invaders);
+  }
+  update() {}
+}
+
+const player = new Player();
+const projectiles = [];
+const grids = [new Grid()];
 const keys = {
   a: {
     pressed: false,
@@ -64,30 +158,42 @@ const keys = {
     pressed: false,
   },
 };
-player.draw();
 
 function animate() {
   requestAnimationFrame(animate);
   c.fillStyle = "black";
   c.fillRect(0, 0, canvas.width, canvas.height);
   player.update();
+  projectiles.forEach((projectile, index) => {
+    if (projectile.position.y + projectile.radius <= 0) {
+      setTimeout(() => {
+        projectiles.splice(index, 1);
+      }, 0);
+    } else {
+      projectile.update();
+    }
+  });
+
+  grids.forEach((grid) => {
+    grid.update();
+    grid.invaders.forEach((invader) => {
+      invader.update();
+    });
+  });
 
   if (
     (keys.a.pressed && player.position.x >= 0) ||
     (keys.ArrowLeft.pressed && player.position.x >= 0)
   ) {
     player.velocity.x = -10;
-    player.rotation = -0.15;
   } else if (
     (keys.d.pressed && player.position.x + player.width <= canvas.width) ||
     (keys.ArrowRight.pressed &&
       player.position.x + player.width <= canvas.width)
   ) {
     player.velocity.x = 10;
-    player.rotation = -0.15;
   } else {
     player.velocity.x = 0;
-    player.rotation = 0;
   }
 }
 animate();
@@ -95,23 +201,31 @@ animate();
 addEventListener("keydown", ({ key }) => {
   switch (key) {
     case "a":
-      console.log("left");
       keys.a.pressed = true;
       break;
     case "ArrowLeft":
-      console.log("left");
       keys.a.pressed = true;
       break;
     case "d":
-      console.log("right");
       keys.d.pressed = true;
       break;
     case "ArrowRight":
-      console.log("right");
       keys.d.pressed = true;
       break;
     case " ":
-      console.log("space");
+      projectiles.push(
+        new Projectile({
+          position: {
+            x: player.position.x + player.width / 2,
+            y: player.position.y,
+          },
+          velocity: {
+            x: 0,
+            y: -10,
+          },
+        })
+      );
+      // console.log(projectiles)
       break;
   }
 });
@@ -119,23 +233,18 @@ addEventListener("keydown", ({ key }) => {
 addEventListener("keyup", ({ key }) => {
   switch (key) {
     case "a":
-      console.log("left");
       keys.a.pressed = false;
       break;
     case "ArrowLeft":
-      console.log("left");
       keys.a.pressed = false;
       break;
     case "d":
-      console.log("right");
       keys.d.pressed = false;
       break;
     case "ArrowRight":
-      console.log("right");
       keys.d.pressed = false;
       break;
     case " ":
-      console.log("space");
       break;
   }
 });
